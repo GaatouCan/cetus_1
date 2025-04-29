d4ff63a45872f005f3d1ebd76060c052c6a84dc3
Astria <1752651025@qq.com>
2025-04-30 01:37:44 +0800

    Initial commit

--- main.go
+++ main.go
@@ -3,9 +3,18 @@ package main
 import (
 	"demo/handler"
 	"github.com/gin-gonic/gin"
+	"golang.org/x/net/context"
+	"log"
+	"net/http"
+	"os/signal"
+	"syscall"
+	"time"
 )
 
 func main() {
+	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	defer stop()
+
 	router := gin.Default()
 
 	helloHandler := handler.HelloHandler{}
@@ -14,8 +23,31 @@ func main() {
 	router.GET("/user", helloHandler.GetUser)
 	router.POST("/user", helloHandler.CreateUser)
 
-	err := router.Run(":8080")
-	if err != nil {
-		return
+	srv := &http.Server{
+		Addr:    ":8080",
+		Handler: router,
 	}
+
+	go func() {
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("listen: %s\n", err)
+		}
+	}()
+
+	<-ctx.Done()
+	stop()
+	log.Print("shutting down server")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatal("Server forced to shutdown:", err)
+	}
+
+	log.Print("Server exiting")
+
+	//err := router.Run(":8080")
+	//if err != nil {
+	//	return
+	//}
 }

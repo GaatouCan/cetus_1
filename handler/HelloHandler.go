ecdf8745c67eb49bfe15121bec620660747153c3
admin <xxx@xx.xom>
2025-04-30 10:12:07 +0800

    Initial commit

--- handler/HelloHandler.go
+++ handler/HelloHandler.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"github.com/gin-gonic/gin"
 	"net/http"
+	"strconv"
 )
 
 type HelloHandler struct{}
@@ -28,7 +29,51 @@ func (h *HelloHandler) CreateUser(c *gin.Context) {
 	var user model.User
 	if err := c.ShouldBindJSON(&user); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
 	}
 	fmt.Println(user)
 	c.JSON(http.StatusOK, user)
+}
+
+func (h *HelloHandler) UpdateUser(c *gin.Context) {
+	idStr := c.Query("id")
+	fmt.Println(idStr)
+
+	id, err := strconv.Atoi(idStr)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	// TODO: Find Target User From Database
+
+	var newUser model.User
+	if err := c.ShouldBindJSON(&newUser); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	if newUser.ID != id {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "id is different"})
+		return
+	}
+
+	// TODO: Update New User To Database
+
+	c.JSON(http.StatusOK, newUser)
+}
+
+func (h *HelloHandler) DeleteUser(c *gin.Context) {
+	idStr := c.Query("id")
+	fmt.Println(idStr)
+
+	id, err := strconv.Atoi(idStr)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	// TODO: Delete Target User From Database
+	message := fmt.Sprintf("Deleted user[%d]", id)
+	c.JSON(http.StatusOK, gin.H{"message": message})
 }
--- main.go
+++ main.go
@@ -12,6 +12,7 @@ import (
 )
 
 func main() {
+	// 创建 OS 信号通道 监听 Ctrl+C 或 kill 信号
 	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
 	defer stop()
 
@@ -22,32 +23,35 @@ func main() {
 	router.GET("/hello", helloHandler.HelloWorld)
 	router.GET("/user", helloHandler.GetUser)
 	router.POST("/user", helloHandler.CreateUser)
+	router.PUT("/user", helloHandler.UpdateUser)
+	router.DELETE("/user", helloHandler.DeleteUser)
 
+	// 创建服务器
 	srv := &http.Server{
 		Addr:    ":8080",
 		Handler: router,
 	}
 
+	// 启动服务在 goroutine 中以便监听 OS 信号
 	go func() {
 		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
 			log.Fatalf("listen: %s\n", err)
 		}
 	}()
 
+	// 等待中断信号触发
 	<-ctx.Done()
 	stop()
 	log.Print("shutting down server")
 
+	// 创建一个 5 秒超时的 context, 用于 graceful shutdown
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
+
+	// 尝试优雅关闭 HTTP 服务器: 停止接受新请求 等待旧请求完成
 	if err := srv.Shutdown(ctx); err != nil {
 		log.Fatal("Server forced to shutdown:", err)
 	}
 
 	log.Print("Server exiting")
-
-	//err := router.Run(":8080")
-	//if err != nil {
-	//	return
-	//}
 }
